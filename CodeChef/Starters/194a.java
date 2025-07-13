ce864c40a920a012156869e38b967d652cc40663
Ankan Basu <ankanbasu10@gmail.com>
2025-07-14 01:15:52 +0530

    Day1

---
+++ CodeChef/Starters/194a.java
@@ -0,0 +1,22 @@
+package CodeChef.Starters;
+
+// Chef Bakes Cake 1
+
+import java.lang.*;
+import java.util.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception     // O(n) and O(1)
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+        int c = N * 30;
+        int r = M * 50;
+        int m = r - c;
+        System.out.println(m);
+        sc.close();
+	}
+}
---
+++ CodeChef/Starters/194b.java
@@ -0,0 +1,39 @@
+package CodeChef.Starters;
+
+//  Chef Bakes Cake 2
+
+
+import java.lang.*;
+import java.util.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		while(t-- > 0){
+		    int N = sc.nextInt();
+		    int A[] = new int[N];
+		    int B[] = new int[N];
+		    
+		    for(int i = 0; i<N; i++){
+		        A[i] = sc.nextInt();
+		    }
+		    for(int i =0; i<N; i++){
+		        B[i] = sc.nextInt();
+		    }
+		    
+		    int c = 0;
+		    for(int i =0; i<N; i++){
+		        if(B[i]<A[i]){      // to handle the case where B[i] is less than A[i], if not handled, it would lead to negative profit
+		            c += A[i] - B[i];
+		        }
+		    }
+		    System.out.println(c);
+		}
+
+	}
+}
